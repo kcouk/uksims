fbda360ad2c0b4218867481f1f67587688577b40
Johnny <johnnymerayo@gmail.com>
2017-12-06 15:43:30 +0000

    Admin UI for administration of spring boot applications
    Change the spring cloud version from Edware to Dalston due compatibility issues

---
+++ boot-admin/src/main/java/co/uk/kuwait/bootadmin/BootAdminApplication.java
@@ -0,0 +1,17 @@
+package co.uk.kuwait.bootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@EnableAdminServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootAdminApplication.class, args);
+	}
+}
