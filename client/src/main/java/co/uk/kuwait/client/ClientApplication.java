fc12b7cf623646e0873972ac51c199e37f1298d5
Johnny <johnnymerayo@gmail.com>
2017-12-11 14:02:24 +0000

    Add client service
    Microproxy all requests

---
+++ client/src/main/java/co/uk/kuwait/client/ClientApplication.java
@@ -0,0 +1,35 @@
+package co.uk.kuwait.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableZuulProxy // Busca en eureka y mapea por nombre del servicio
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+}
+
+
+//Test to check remote Cloud Config
+@RefreshScope
+@RestController
+class MessageRestController {
+
+	@Value("${server.port:Hello default client}")
+	private String message;
+
+	@RequestMapping("/message")
+	String getMessage() {
+		return this.message;
+	}
+}
